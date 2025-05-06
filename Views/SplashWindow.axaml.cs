9d5cd9d5721b7de8acd36178a4cefca271efc276
Viktor Slobodianyk <viktor-sbdn@proton.me>
2025-05-06 20:17:36 +0200

    Add SplashWindow implementation

---
+++ Views/SplashWindow.axaml.cs
@@ -0,0 +1,39 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Threading;
+using System;
+using System.Threading.Tasks;
+
+namespace CADA.Views;
+
+public partial class SplashWindow : Window
+{
+    private readonly Action? _mainAction;
+
+    public SplashWindow()
+    {
+    }
+
+    public SplashWindow(Action mainAction)
+    {
+        InitializeComponent();
+        _mainAction = mainAction;
+    }
+
+    protected override void OnLoaded(RoutedEventArgs e)
+    {
+        DummyLoad();
+    }
+
+    private async void DummyLoad()
+    {
+        // Do some background stuff here.
+        await Task.Delay(3000);
+
+        await Dispatcher.UIThread.InvokeAsync(() =>
+        {
+            _mainAction?.Invoke();
+            Close();
+        });
+    }
+}
